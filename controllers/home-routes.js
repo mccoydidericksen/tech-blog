df8de6707635144082b15ce6c229c6b91285d6ad
McCoy Didericksen <mccoy.did@gmail.com>
2023-02-09 10:26:34 -0700

    add home routes and auth middleware

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,85 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models/index');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'date_created', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+        const post = dbPostData.get({ plain: true });
+        res.render('post', {
+            post,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id,
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render('dashboard', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
 
 module.exports = router;
