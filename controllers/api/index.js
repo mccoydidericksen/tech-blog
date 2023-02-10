9fc294b971f3c25eebdf7797ad1ac517d3e71fcf
McCoy Didericksen <mccoy.did@gmail.com>
2023-02-09 17:06:03 -0700

    add index.js for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
